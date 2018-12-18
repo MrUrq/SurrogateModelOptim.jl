f10eb682ddaace32ad83f7c8936063c8146a6d43
autodocs <autodocs>
2018-12-18 09:12:46 +0000

    build based on 333d8c0

--- latest/search_index.js
+++ latest/search_index.js
@@ -9,6 +9,38 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "index.html#SurrogateModelOptim._scale-Union{Tuple{T}, Tuple{Array{T,1},Any,Any}} where T<:Real",
+    "page": "Home",
+    "title": "SurrogateModelOptim._scale",
+    "category": "method",
+    "text": "function _scale(oldX::Array{T,1},newMin,newMax;oldMin=minimum(oldX), oldMax=maximum(oldX)) where T <: Real\n\nScale a vector to match a new range.\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#SurrogateModelOptim._scale-Union{Tuple{T}, Tuple{Array{T,2},Int64,Any,Any}} where T<:Real",
+    "page": "Home",
+    "title": "SurrogateModelOptim._scale",
+    "category": "method",
+    "text": "function _scale(oldX::Array{T,2},direction::Int,newMin,newMax; oldMin=minimum(oldX,direction)::Array{T,2},oldMax=maximum(oldX,direction)::Array{T,2}) where T <: Real \n\nScale a 2D matrix to match a new range along the specified direction.\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#SurrogateModelOptim._scale-Union{Tuple{T}, Tuple{T,Any,Any}} where T<:Real",
+    "page": "Home",
+    "title": "SurrogateModelOptim._scale",
+    "category": "method",
+    "text": "function _scale(oldX::T,newMin,newMax;oldMin=minimum(oldX), oldMax=maximum(oldX)) where T <: Real\n\nScale a scalar to match a new range.\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#SurrogateModelOptim._setup_parameters",
+    "page": "Home",
+    "title": "SurrogateModelOptim._setup_parameters",
+    "category": "function",
+    "text": "_setup_parameters(SearchRange, NumDimensions=false; kwargs...)\n\nInternal function of SurrogateModelOptim to handle the interface which constrains  the input combinations the user can use. \n\n\n\n\n\n"
+},
+
+{
     "location": "index.html#SurrogateModelOptim.jl-1",
     "page": "Home",
     "title": "SurrogateModelOptim.jl",
