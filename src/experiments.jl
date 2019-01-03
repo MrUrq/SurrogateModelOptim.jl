2033ee7d74621e418f4e4d5a6b4db971050ecf21
Magnus Urquhart <magnus.urquhart@gmail.com>
2019-01-03 13:11:19 +0100

    Updated for comparison with smoothing branch

--- src/experiments.jl
+++ src/experiments.jl
@@ -48,7 +48,7 @@ ax = funcplotX(styblinskiTang,-5,5,-5,5)
 
 @time asd = smoptimize(x->(styblinskiTang(x)+0*rand(MersenneTwister(abs(sum(reinterpret(Int64,x)))))), [(-5.0,5.0),(-5.0,5.0)],SurrogateModelOptim.options(num_interpolants=20, num_start_samples=10, sampling_plan_opt_gens=100000, rbf_opt_gens=25000,  variable_kernel_width = true,
                                                                                                        variable_dim_scaling = true,
-                                                                                                       smooth = false));
+                                                                                                       ));
 
 ax = funcplotX(x->median(asd[3](x))[1],-5,5,-5,5)
 
