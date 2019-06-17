71e83f9b53e1bd3f50a8113b1f0b826cacc42e43
Magnus Urquhart <magnus.urquhart@gmail.com>
2019-06-17 18:28:54 +0200

    Updated travis requirements to julia 1.1

--- docs/make.jl
+++ docs/make.jl
@@ -2,20 +2,19 @@ using Documenter, SurrogateModelOptim
 
 makedocs(;
     modules=[SurrogateModelOptim],
-    format=:html,
+    format = Documenter.HTML(),
     pages=[
         "Home" => "index.md",
     ],
     repo="https://github.com/MrUrq/SurrogateModelOptim.jl/blob/{commit}{path}#L{line}",
     sitename="SurrogateModelOptim.jl",
     authors="Magnus Urquhart",
-    assets=[],
+    #assets=[],
 )
 
 deploydocs(;
     repo="github.com/MrUrq/SurrogateModelOptim.jl",
     target="build",
-    julia="1.0",
     deps=nothing,
     make=nothing,
 )
