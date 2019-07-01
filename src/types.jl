d8e384d9ee4cfe911427b0ed0f64b73d82cba6c8
Magnus Urquhart <magnus.urquhart@gmail.com>
2019-07-01 13:14:46 +0200

    Updated with better default options

--- src/types.jl
+++ src/types.jl
@@ -6,7 +6,7 @@ Options configurable by the user with recommended default values.
 @with_kw struct Options
     num_start_samples::Int = 5
     trace::Bool = true
-    sampling_plan_opt_gens::Int = 5000
+    sampling_plan_opt_gens::Int = 50_000
     rippa::Bool = true
     kerns = [ScatteredInterpolation.Gaussian, ScatteredInterpolation.InverseQuadratic,
              ScatteredInterpolation.InverseMultiquadratic]
@@ -16,16 +16,16 @@ Options configurable by the user with recommended default values.
     rbf_dist_metric = Distances.Euclidean()
     variable_kernel_width::Bool = true
     variable_dim_scaling::Bool = true
-    cond_max::Float64 = 1e4
-    max_rbf_width::Float64 = 1000.0
-    min_rbf_width::Float64 = 1e-4
-    max_scale::Float64 = 10.0
-    min_scale::Float64 = 1e-4
-    num_interpolants::Int = 20
-    smooth = :single
-    max_smooth::Float64 = 1.0
+    cond_max::Float64 = 1e10
+    max_rbf_width::Float64 = 1.0
+    min_rbf_width::Float64 = 1e-10
+    max_scale::Float64 = 1.0
+    min_scale::Float64 = 1e-10
+    num_interpolants::Int = 10
+    smooth = false
+    max_smooth::Float64 = 0.005
     smooth_user::Float64 = 0.0
-    iterations::Int64 = 10
+    iterations::Int64 = 5
     num_infill_points::Int64 = 1
     parallel_surrogate::Bool = true
     infill_funcs::Array{Symbol,1} = [:median,:std]
